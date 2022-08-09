9b2133053fdceaf264762fdb2cdc219c5d06e2e2
Alessandro Bridi <ale.bridi15@gmail.com>
2022-08-09 18:57:28 +0200

    kernel modified

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,14 +2,8 @@
 
 #define VIDEO_MEM 0xb8000
 
-void set_char_at_video_memory_k(char character, int offset){
-	unsigned char *vidmem = (unsigned char *) VIDEO_MEM;
-	vidmem[offset] = character;
-	vidmem[offset+1] = WHITE_ON_BLACK;
-}
-
 int main(){
 	unsigned char *vidmem = (unsigned char*) VIDEO_MEM;
 
-	set_char_at_video_memory_k('X', 161);
+	print_string("--Da kernel--");
 }
