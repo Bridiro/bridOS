d028ac141830545016fa8dd0082f7b1d2674ca42
Bridiro <ale.bridi15@gmail.com>
2025-01-11 18:36:19 +0100

    chore: updated main view

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -7,8 +7,8 @@ void start_kernel() {
     init_dynamic_mem();
     init_display();
 
-    fill_screen(0xffff00ff);
+    fill_screen(0xffa3a3a3);
     draw_rectangle(100, 100, 100, 100, 0xffffffff);
-    draw_rectangle(250, 100, 100, 100, 0x00000000);
+    draw_rectangle(100, 250, 100, 100, 0x00000000);
     draw_text(210, 100, LEFT, "Hello, World", 0xff000000, 1.0, put_pixel);
 }
